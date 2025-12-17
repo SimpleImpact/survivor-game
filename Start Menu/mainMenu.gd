36a6e4cac142b829e7b06f098788fad2675009e7
SimpleImpact <gwalters5926@gmail.com>
2025-12-17 11:16:27 -0600

    Organizational Update
    Relocated main menu script, scene, and related resources

---
+++ Start Menu/mainMenu.gd
@@ -0,0 +1,16 @@
+extends Control
+
+@onready var bg = $Background
+#@onready var noise = load("res://mainMenuNoise.tres")
+
+func _ready() -> void:
+	pass
+
+func startButton() -> void:
+	pass # Replace with function body.
+
+func settingsButton() -> void:
+	pass # Replace with function body.
+
+func quitButton() -> void:
+	pass # Replace with function body.
--- mainMenu.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-@onready var bg = $Background
-
-func _ready() -> void:
-	bg.texture.noise.Gain = 1.0
